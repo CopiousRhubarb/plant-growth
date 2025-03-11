8888f1d16caf48ea4d4955b6e277f45f4a0b4cd7
cjlim1092 <32859465+cjlim1092@users.noreply.github.com>
2025-03-10 21:24:26 -0700

    Update PlantGrowth.java

--- PlantGrowth.java
+++ PlantGrowth.java
@@ -1,5 +1,30 @@
+// Purpose
+// Lately I have been getting into growing plants from seed. This is a process that is very daunting, but rewarding. I want to see how different conditions would effect the growth of my plants, such as how much sunlight and water the plant recieves. 
+// So this program will be used to simulate seedling growth under different conditions. 
+
+// Collaborators: Ryan Moore and Caitlin Lim
+
 public class PlantGrowth {
     public static void main(String[] args) {
 
+
+//DECLARATION SECTION
+    int week; 
+    int sunlight; //full, partial, and shaded will be calculated separately
+    int water; //top and bottom watered will be calculated in addition to sunlight
+
+//INITIALIZATION SECTION
+
+//INPUT + CALCULATION SECTION
+
+//method 1: calculate growth under different sunlight conditions
+
+//method 2: calculate growth under different watering conditions 
+
+//mathod 3: drawing seed
+
+//method 4: introducing the user to the project
+
+//OUTPUT SECTION
     }
 }
