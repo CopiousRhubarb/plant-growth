3c0d8b1c12533569bc10cc3f76f1fbda99b9500f
cjlim1092 <32859465+cjlim1092@users.noreply.github.com>
2025-03-11 20:13:49 -0700

    Update PlantGrowth.java
    Adding water conditions method - might need debugging

--- PlantGrowth.java
+++ PlantGrowth.java
@@ -8,22 +8,47 @@ public class PlantGrowth {
     public static void main(String[] args) {
 
         // DECLARATION SECTION
-        int week;
-        int sunlight; // full, partial, and shaded will be calculated separately
-        int water; // top and bottom watered will be calculated in addition to sunlight
+        int week; //weeks from 0 to 8
+        int sunFull; // full, partial, and shaded will be calculated separately
+        int sunPartial;
+        int sunShaded;
+        int waterTop; // top and bottom watered will be calculated in addition to sunlight
+        int waterBottom;
 
         // INITIALIZATION SECTION
 
-        // INPUT + CALCULATION SECTION
+        // CALCULATION SECTION
 
         // method 1: calculate growth under different sunlight conditions - Ryan
 
         // method 2: calculate growth under different watering conditions - Caitlin
+        calculateWaterConditions();
 
         // mathod 3: drawing seed - Ryan
 
         // method 4: introducing the user to the project - Caitlin
 
         // OUTPUT SECTION
+    }
+    
+    // method 2: calculate growth under different watering conditions - Caitlin
+    //for each sunlight condition there are 2 watering conditions waterTop will have an double of .9 and waterBottom will have an int of 1  
+    public static void calculateWaterConditions()
+    {
+        int waterTop = (sunFull);
+        int waterBottom = (sunFull+1);
+        system.out.println("The plant grew "+ waterTop+" inches when watering from above in full sunlight.");
+        system.out.println("The plant grew "+ waterBottom+" inches when watering from below in full sunlight.");
+
+        int waterTop = (sunPartial);
+        int waterBottom = (sunPartial+1);
+        system.out.println("The plant grew "+ waterTop+" inches when watering from above in partial sunlight.");
+        system.out.println("The plant grew "+ waterBottom+" inches when watering from below in partial sunlight.");
+
+        int waterTop = (sunShaded);
+        int waterBottom = (sunShaded+1);
+        system.out.println("The plant grew "+ waterTop+" inches when watering from above in the shade.");
+        system.out.println("The plant grew "+ waterBottom+" inches when watering from below in the shade.");
+
     }
 }
